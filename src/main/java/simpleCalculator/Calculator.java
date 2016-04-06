c41edf5dbf14d92bed9ce05dc7e61306e200d407
ischung <insang@hansung.ac.kr>
2016-04-06 16:03:00 +0900

    sub 기능 추가
    sub기능과 테스트 코드 추가

--- src/main/java/simpleCalculator/Calculator.java
+++ src/main/java/simpleCalculator/Calculator.java
@@ -7,6 +7,10 @@ public class Calculator {
 		result=x+y;
 	}
 	
+	public void sub(int x, int y) {
+		result=x-y;
+	}
+	
 	public int getResult() {
 		return result;
 	}
--- src/test/java/simpleCalculator/CalculatorTest.java
+++ src/test/java/simpleCalculator/CalculatorTest.java
@@ -12,5 +12,12 @@ public class CalculatorTest {
 		calc.add(10, 20);
 		assertEquals(30, calc.getResult());
 	}
+	
+	@Test
+	public void testSub() {
+		Calculator calc = new Calculator();
+		calc.sub(10, 20);
+		assertEquals(-10, calc.getResult());
+	}
 
 }
